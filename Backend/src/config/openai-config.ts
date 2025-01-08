61f24868fafe695581b5f7d51372faf5ad6a145d
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-08 19:26:28 +0800

    basic CloseAI- GPT use
    basic CloseAI- GPT use

--- Backend/src/config/openai-config.ts
+++ Backend/src/config/openai-config.ts
@@ -1,12 +1,11 @@
 import { Configuration } from "openai";
 
 export const configOpenAI = () => {
+  // closeAI连接，可通过改为OPENAI正常连接
   const apiKey = process.env.OPENAI_GUONEI_KEY;
-  const organization = process.env.OPENAI_ORGANIZATION_ID;
   const URL = process.env.OPENAI_GUONEI_BASE_URL;
   const config = new Configuration({
     apiKey,
-    organization,
     basePath: URL,
   });
   return config;
