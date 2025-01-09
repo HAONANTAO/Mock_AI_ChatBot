cb3829b48d83c9e5691b58992346d411057f6192
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-09 14:23:12 +0800

    restore to the start

--- Frontend/src/components/chat/ChatItem.tsx
+++ Frontend/src/components/chat/ChatItem.tsx
@@ -1,100 +1,100 @@
-import { Avatar, Box, Typography } from "@mui/material";
 import React from "react";
+import { Box, Avatar, Typography } from "@mui/material";
 import { useAuth } from "../../context/AuthContext";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-// color
 import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { franc } from "franc";
-// 定义 ChatItemProps 类型
-type ChatItemProps = {
+
+function extractCodeFromString(message: string) {
+  if (message.includes("```")) {
+    const blocks = message.split("```");
+    return blocks;
+  }
+}
+
+function isCodeBlock(str: string) {
+  if (
+    str.includes("=") ||
+    str.includes(";") ||
+    str.includes("[") ||
+    str.includes("]") ||
+    str.includes("{") ||
+    str.includes("}") ||
+    str.includes("#") ||
+    str.includes("//")
+  ) {
+    return true;
+  }
+  return false;
+}
+const ChatItem = ({
+  content,
+  role,
+}: {
   content: string;
   role: "user" | "assistant";
-};
-// 从给出来的字符串中间拿到code代码部分
-function extractCodeFromString(message: string) {
-  const codeBlocks = [];
-  const regex = /```([\s\S]*?)```/g;
-  let match;
-  while ((match = regex.exec(message)) !== null) {
-    codeBlocks.push(match[1]);
-  }
-  return codeBlocks;
-}
-
-// detect the code languages part
-function detectLanguage(block: string): string {
-  const cleanBlock = block.replace(/[^a-zA-Z0-9]/g, " ").trim();
-  const lang = franc(cleanBlock);
-  console.log(lang);
-  return lang || "text";
-}
-
-//
-const ChatItem = ({ content, role }: ChatItemProps) => {
-  const auth = useAuth();
-  // 可能有也可能没有
+}) => {
   const messageBlocks = extractCodeFromString(content);
-  return role === "assistant" ? (
-    <>
-      {/* 电脑的回复part */}
-      <Box
-        sx={{
-          display: "flex",
-          p: 2,
-          bgcolor: "#004d5612",
-          my: 2,
-          gap: 2,
-        }}>
-        <Avatar sx={{ ml: 0 }}>
-          <img src="openai.png" alt="openai png" width="30px" />
-        </Avatar>
-
-        {/* messages reply */}
-        <Box>
-          {/* 看有没有代码显示区别 */}
-          {!messageBlocks && <Typography fontSize="20px">{content}</Typography>}
-          {messageBlocks &&
-            messageBlocks.length > 0 &&
-            messageBlocks.map((block, index) => {
-              const lang = detectLanguage(block);
-              return lang === "text" ? (
-                <Typography fontSize="20px">{block}</Typography>
-              ) : (
-                <SyntaxHighlighter
-                  language={lang}
-                  style={coldarkDark}
-                  key={index}>
-                  {block}
-                </SyntaxHighlighter>
-              );
-            })}
-        </Box>
+  const auth = useAuth();
+  return role == "assistant" ? (
+    <Box
+      sx={{
+        display: "flex",
+        p: 2,
+        bgcolor: "#004d5612",
+        gap: 2,
+        borderRadius: 2,
+        my: 1,
+      }}>
+      <Avatar sx={{ ml: "0" }}>
+        <img src="openai.png" alt="openai" width={"30px"} />
+      </Avatar>
+      <Box>
+        {!messageBlocks && (
+          <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+        )}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter style={coldarkDark} language="javascript">
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography sx={{ fontSize: "20px" }}>{block}</Typography>
+            ),
+          )}
       </Box>
-    </>
+    </Box>
   ) : (
-    <>
-      {/* 我的询问列表part */}
-      <Box
-        sx={{
-          display: "flex",
-          p: 2,
-          bgcolor: "#004d56",
-
-          gap: 2,
-        }}>
-        <Avatar sx={{ ml: 0, bgcolor: "black", color: "white" }}>
-          {/* 名字首字母 */}
-          {auth?.user?.name[0]}
-          {/* 万一没有第二个last name  */}
-          {auth?.user?.name?.split(" ")?.[1]?.[0] ?? ""}
-        </Avatar>
-
-        {/* messages reply */}
-        <Box>
-          <Typography fontSize="20px">{content}</Typography>
-        </Box>
+    <Box
+      sx={{
+        display: "flex",
+        p: 2,
+        bgcolor: "#004d56",
+        gap: 2,
+        borderRadius: 2,
+      }}>
+      <Avatar sx={{ ml: "0", bgcolor: "black", color: "white" }}>
+        {auth?.user?.name[0]}
+        {auth?.user?.name.split(" ")[1][0]}
+      </Avatar>
+      <Box>
+        {!messageBlocks && (
+          <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+        )}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter style={coldarkDark} language="javascript">
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography sx={{ fontSize: "20px" }}>{block}</Typography>
+            ),
+          )}
       </Box>
-    </>
+    </Box>
   );
 };
 
