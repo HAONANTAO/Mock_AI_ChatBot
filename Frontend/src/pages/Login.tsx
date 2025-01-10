3a0174cb9c408ceedb4e0bc09b6d1b643bf13956
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-10 14:29:19 +0800

    protected routes in login goto chat
    protected routes in login goto chat

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,10 +1,12 @@
 import { Box, Typography, Button } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import CustomisedInput from "../components/shared/CustomisedInput";
 import { BiLogInCircle } from "react-icons/bi";
 import { toast } from "react-hot-toast";
 import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const navigator = useNavigate();
   const auth = useAuth();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -22,6 +24,12 @@ const Login = () => {
       toast.error("Signed in Failed!", { id: "login" });
     }
   };
+  useEffect(() => {
+    if (auth?.user) {
+      navigator("/chat");
+      return;
+    }
+  }, [auth]);
   return (
     <>
       <Box width={"100%"} height={"100%"} display="flex" flex="1">
