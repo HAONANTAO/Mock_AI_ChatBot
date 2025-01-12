d3c51b439b18ae87d219cfb2937cf204af714b1e
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-12 22:26:55 +0800

    update deploy.sh
    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-# 进入前端文件夹，安装依赖并构建
-if [ -d "Frontend" ]; then
-    echo "Entering Frontend folder"
-    cd Frontend
-    npm install || exit 1
-    npm run build || exit 1
-    echo "Frontend build completed, exiting Frontend folder"
-    cd..
+if[ -d "Frontend"];then
+  echo "Enter Frontend folder"
+  cd Frontend
+  npm install || exit 1
+  npm run build || exit 1
+  echo "Frontend build good,exited"
+  cd ..
 else
-    echo "Frontend folder does not exist."
-    exit 1
+  echo "Frontend folder not existed"
+  exit 1
 fi
 
-# 进入后端文件夹，安装依赖并启动服务
-if [ -d "Backend" ]; then
-    echo "Entering Backend folder"
-    cd Backend
-    npm install || exit 1
-    npm start || exit 1
-    echo "Backend service started, exiting Backend folder"
-    cd..
+if[-d "Backend"];then
+  echo "Entering Backend folder"
+  cd Backend
+  npm install || exit 1
+  npm start || exit 1
+  echo "Backend service started"
+  cd..
 else
-    echo "Backend folder does not exist."
-    exit 1
+  echo "Backend folder not existed"
+  exit 1
 fi
