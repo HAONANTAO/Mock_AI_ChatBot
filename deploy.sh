802266f5b6c05c5f9eb51b835a71b54b0aa71848
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-12 21:48:18 +0800

    try differ deploy
    try differ deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 # 进入前端文件夹，安装依赖并构建
-cd Backend
-npm install
-npm run build
-cd..
+if [ -d "Frontend" ]; then
+    echo "Entering Frontend folder"
+    cd Frontend
+    npm install || exit 1
+    npm run build || exit 1
+    echo "Frontend build completed, exiting Frontend folder"
+    cd..
+else
+    echo "Frontend folder does not exist."
+    exit 1
+fi
+
 # 进入后端文件夹，安装依赖并启动服务
-cd Frontend
-npm install
-npm start
-cd..
+if [ -d "Backend" ]; then
+    echo "Entering Backend folder"
+    cd Backend
+    npm install || exit 1
+    npm start || exit 1
+    echo "Backend service started, exiting Backend folder"
+    cd..
+else
+    echo "Backend folder does not exist."
+    exit 1
+fi
