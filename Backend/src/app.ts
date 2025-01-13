0b395cabad12978f39b89a31216ad8c90c8f486d
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-13 21:58:37 +0800

    serverless
    serverless

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
-// 从 dotenv 库中引入的 config 函数，在执行时会自动查找项目根目录下名为 .env 的文件
+// 从 dotenv 库中引入的 config 函数，在执行时会自动查找项目根目录下名为.env 的文件
 import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 config();
-// 创建app
+// 创建 app
 const app = express();
 // 跨域
 app.use(
@@ -17,10 +17,13 @@ app.use(morgan("dev"));
 // middlewares
 // 配置用于解析 application/json 格式数据的中间件
 app.use(express.json());
-
 // 配置用于解析 application/x-www-form-urlencoded 格式数据的中间件（表单数据）
 app.use(express.urlencoded({ extended: true }));
-// 解析cookies,加密
+// 解析 cookies，加密
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use("/api/v1", appRouter);
-export default app;
+
+// 导出处理函数
+export default function (req, res) {
+  app(req, res);
+}
