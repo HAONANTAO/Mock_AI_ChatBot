7405abb94962545f17b6a5472aae2d01f5eae982
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-03-11 16:57:06 +1100

    Update Chat.tsx

--- Frontend/src/pages/Chat.tsx
+++ Frontend/src/pages/Chat.tsx
@@ -116,14 +116,14 @@ const Chat = () => {
                 color: "black",
                 fontWeight: 700,
               }}>
-              {typeof auth?.user?.name === "string" && auth.user.name.length > 0
+              {/* {typeof auth?.user?.name === "string" && auth.user.name.length > 0
                 ? auth.user.name[0]
                 : ""}
               {typeof auth?.user?.name === "string" &&
               auth.user.name.includes(" ") &&
               auth.user.name.split(" ")[1]
                 ? auth.user.name.split(" ")[1][0]
-                : ""}
+                : ""} */}
             </Avatar>
 
             {/* text */}
